09f8a525cb0efc94dc68b8335877cc052247c008
Alejandro Gil <alejandro_gilcoronado>
2026-03-11 16:28:38 +0100

    Canvi vista crear alumne

--- resources/views/alumnes/create.blade.php
+++ resources/views/alumnes/create.blade.php
@@ -14,7 +14,7 @@
         <header class="bg-white shadow">
             <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                 <h2 class="font-semibold text-xl text-gray-800">
-                    Nou Alumne
+                    Nou Alumne +
                 </h2>
             </div>
         </header>
